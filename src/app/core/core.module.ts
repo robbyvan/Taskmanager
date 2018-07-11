be84778ff37fd0761132b6031712c38a57602fce
Robby <fzpinus@gmail.com>
2018-07-11 21:14:48 +0800

    feat(sidebar): mat-icon added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,10 +7,16 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { loadSvgResources } from '../utils/svg.utils';
+
 @NgModule({
   imports: [
     SharedModule,
     BrowserAnimationsModule,
+    HttpClientModule,
   ],
   exports: [
     HeaderComponent,
@@ -21,9 +27,14 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   declarations: [HeaderComponent, FooterComponent, SidebarComponent]
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+  constructor(
+    @Optional() @SkipSelf() parent: CoreModule,
+    ir: MatIconRegistry,
+    ds: DomSanitizer,
+  ) {
     if (parent) {
       throw new Error('Core module already exists.');
     }
+    loadSvgResources(ir, ds);
   }
 }
