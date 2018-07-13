298e26780ae739af5ddaac4bebed7446f764c4c8
Robby <fzpinus@gmail.com>
2018-07-13 18:16:18 +0800

    feat(forms): model driven form - login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    // this.loginForm = new FormGroup({
+    //   email: new FormControl('van@qq.com', Validators.compose([Validators.required, Validators.email])),
+    //   password: new FormControl('', Validators.required),
+    // });
+    this.loginForm = this.fb.group({
+      email: ['van@qq.com', Validators.compose([Validators.required, Validators.email, this.validate])],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(form, e: Event) {
+    e.preventDefault();
+    console.log(form);
+    const { value, valid } = form;
+    console.log(JSON.stringify(value));
+    console.log(JSON.stringify(valid));
+  }
+
+  validate(fc: FormControl): {[key: string]: any} {
+    if (!fc.value) {
+      return null;
+    }
+    const pattern = /^van+/;
+    if (pattern.test(fc.value)) {
+      return null;
+    }
+    return {
+      emailNotValid: 'Email must start with "van"'
+    };
   }
 
 }
