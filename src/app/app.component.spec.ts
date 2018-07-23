de3e9ad2a0d4f4896c9f51acda77827e723cda67
Robby <fzpinus@gmail.com>
2018-07-23 21:56:33 +0800

    test(auth reducer): auth reducer test added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,40 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material';
+import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        CoreModule,
+        RouterModule.forRoot([]),
+        MatSidenavModule,
+      ],
+      providers: [
+        {
+          provide: APP_BASE_HREF,
+          useValue: '/'
+        }
+      ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('应该创建应用', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('应该有.site元素', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('.site').textContent).toBeTruthy();
   }));
 });
